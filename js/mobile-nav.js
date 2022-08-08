510b768484edc418616780ddeb5afb1a1f3ef758
francesm <fr.ncesm@gmail.com>
2022-08-07 21:55:00 -0600

    mobile nav closes on nav link click :alien:

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -3,18 +3,20 @@ const nav = {};
 nav.buttonEl = document.querySelector('.side-nav--mobile');
 nav.mobileNavEl = document.querySelector('.header__mobile-nav');
 nav.socialLinksEl = document.querySelector('.side-nav--desktop');
+nav.navLinksEl = document.querySelectorAll('.header__mobile-nav-links a');
 
-nav.openNav = () => {
-    nav.buttonEl.addEventListener('click', () => {
-        nav.mobileNavEl.classList.toggle('open');
-        nav.socialLinksEl.classList.toggle('show');
-        nav.buttonEl.classList.toggle('slide');
-    });
+nav.toggleNav = () => {
+    nav.mobileNavEl.classList.toggle('open');
+    nav.socialLinksEl.classList.toggle('show');
+    nav.buttonEl.classList.toggle('slide');
 }
 
-
 nav.init = () => {
-    nav.openNav();
+    nav.buttonEl.addEventListener('click', nav.toggleNav);
+
+    nav.navLinksEl.forEach((linkEl) => {
+        linkEl.addEventListener('click', nav.toggleNav);
+    })
 }
 
 

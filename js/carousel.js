8434678b34400010d5905c0580a85b01a06c57be
francesm <fr.ncesm@gmail.com>
2022-09-08 20:00:10 -0600

    continuing carousel logic/styling

--- js/carousel.js
+++ js/carousel.js
@@ -1,101 +1,152 @@
+//import { tns } from "../node_modules/tiny-slider/src/tiny-slider.js";
 const carousel = {};
 
 carousel.projects = [
     {
-        imgSrc: "./assets/images/projects/moneta.png",
+        id: "moneta",
+        desktopPreview: "./assets/images/projects/moneta.png",
+        mobilePreview: "./assets/images/projects/moneta-mobile.png",
         alt: "screenshot of a budgeting app",
         title: "Budgeting App",
-        skillsUsed: "React, Firebase",
-        description: "",
+        skillsUsed: "React, Firebase, CSS",
+        description: "Created at <a href='https://junocollege.com/' target='_blank' rel='noopener noreferrer'>Juno College</a> - a budgeting app with persistent data that allows users to create an account and save their personal budget.",
         liveUrl: "https://francesm-budget-app.netlify.app/",
         repoUrl: "https://github.com/frances-m/budget-app"
     },
     {
-        imgSrc: "./assets/images/projects/calculator-app.png",
+        id: "calc",
+        desktopPreview: "./assets/images/projects/calculator-app.png",
+        mobilePreview: "./assets/images/projects/calculator-app-mobile.png",
         alt: "screenshot of a calculator app",
         title: "Calculator App",
-        skillsUsed: "HTML, SCSS, JavaScript, jQuery",
-        description: "A fully functional calculator app with keyboard functionality. This design comes from <a href='https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29'>Frontend Mentor</a> and features several themes. Once the user selects a theme, the app stores the users preferred theme and automatically displays it on subsequent visits",
+        skillsUsed: "jQuery, JavaScript, SCSS",
+        description: "A fully functional calculator app with keyboard functionality and multiple themes. This design comes from <a href='https://www.frontendmentor.io/challenges/calculator-app-9lteq5N29' target='_blank' rel='noopener noreferrer'>Frontend Mentor</a>.",
         liveUrl: "https://francesm-calculator-app.netlify.app/",
         repoUrl: "https://github.com/frances-m/calculator-app"
     },
     {
-        imgSrc: "./assets/images/projects/simpsons-trivia.png",
+        id: "simpsons",
+        desktopPreview: "./assets/images/projects/simpsons-trivia.png",
+        mobilePreview: "./assets/images/projects/simpsons-trivia-mobile.png",
         alt: "screenshot of a simpsons trivia app",
-        title: "Simpsons Quote Trivia",
-        skillsUsed: "HTML, CSS, JavaScript, REST API",
-        description: "A paired programming project, made at <a href='https://junocollege.com/'>Juno College</a>. This is a fun trivia game made using data from a Simpsons Quote API. Users are presented with a quote and asked to select the character that the quote belongs to. The users score is tracked and a final score is presented at the end.",
+        title: "Simpsons Trivia",
+        skillsUsed: "REST API, JavaScript",
+        description: "A paired programming project, made at <a href='https://junocollege.com/' target='_blank' rel='noopener noreferrer'>Juno College</a>. A fun trivia game made using data from a Simpsons Quote API! Users are presented with a quote and asked to select the character that the quote belongs to.",
         liveUrl: "https://simpsons-quote-trivia.netlify.app/",
         repoUrl: "https://github.com/paired-project/simpsons-quote-trivia"
     },
     {
-        imgSrc: "./assets/images/projects/creative-design.png",
+        id: "creative",
+        desktopPreview: "./assets/images/projects/creative-design.png",
+        mobilePreview: "./assets/images/projects/creative-design-mobile.png",
         alt: "screenshot of a design agency website",
         title: "Creative Design",
-        skillsUsed: "HTML, SCSS, JavaScript",
-        description: "A responsive multi-page site following design documents provided by <a href='https://junocollege.com/'>Juno College</a>. Some of the functionality highlights include the main page carousel, the blog page comments form, and the mobile nav.",
+        skillsUsed: "JavaScript, SCSS, HTML",
+        description: "A responsive multi-page site following design documents provided by <a href='https://junocollege.com/' target='_blank' rel='noopener noreferrer'>Juno College</a>. Some of the functionality highlights include the blog page comments form and the mobile nav.",
         liveUrl: "https://francesm-creative-design.netlify.app/",
         repoUrl: "https://github.com/frances-m/creative-design-site"
     }
 ];
 
-carousel.buttonEls = document.querySelectorAll(".projects__carousel-btn");
+carousel.rightButtonEl = document.querySelector(".projects__carousel-btn--right");
+carousel.leftButtonEl = document.querySelector(".projects__carousel-btn--left");
 carousel.carouselEl = document.querySelector(".projects__carousel");
 
-carousel.position = 0;
+carousel.isMobileView = false;
+
+carousel.createCarousel = () => {
+    carousel.populateCarousel();
+
+    const carouselOptions = tns({
+        container: '.projects__carousel',
+        items: 1,
+        center: true,
+        loop: true,
+        speed: 400,
+        mouseDrag: true,
+        gutter: 20,
+        controls: true,
+        prevButton: carousel.leftButtonEl,
+        nextButton: carousel.rightButtonEl,
+        nav: true,
+        navPosition: 'bottom',
+        //navAsThumbnails: true,
+        autoWidth: true,
+        responsive: {
+            
+        }
+    })
+}
 
 carousel.populateCarousel = () => {
-
+    carousel.carouselEl.innerHTML = '';
+    
     carousel.projects.forEach((project) => {
+        //const projectPreview = carousel.isMobileView ? project.mobilePreview : project.desktopPreview;
+        const projectPreview = project.mobilePreview;
         const projectListEl = document.createElement("li");
-        projectListEl.classList.add("projects__project-container");
+        projectListEl.classList.add("projects__project");
 
         projectListEl.innerHTML = `
-            <div class="projects__project">
-                <div class="projects__img-container" style="background-image: url(${project.imgSrc});">
-                    
-                </div> <!-- END project__img-container -->
+            <div class="projects__img-container">
+                <img src=${projectPreview} alt=${project.alt} />
+            </div> <!-- END project__img-container -->
+            <div class="projects__text-bg projects__${project.id}">
                 <div class="projects__text-container">
+                    <button class="projects__open-text" value=${project.id}>
+                        <span></span>
+                    </button>
                     <h5>${project.title}</h5>
-                    <p class="projects__built-with">
+                    <p class="projects__tech">
                         [${project.skillsUsed}]
                     </p>
                     <p class="projects__description">
                         ${project.description}
                     </p>
                     <div class="projects__btn-container">
-                        <a href=${project.liveUrl} class="button" target="_blank">View Live</a>
-                        <a href=${project.repoUrl} class="button" target="_blank">View Repo</a>
+                        <a href=${project.liveUrl} class="button" target="_blank" rel="noopener noreferrer">View Live</a>
+                        <a href=${project.repoUrl} class="button" target="_blank" rel="noopener noreferrer">View Repo</a>
                     </div> <!-- END projects__btn-container -->
                 </div> <!-- END projects__text-container -->
-            </div>
+            </div> <!-- END projects__text-bg -->
         `;
 
         carousel.carouselEl.append(projectListEl);
     });
 }
 
+carousel.setView = () => {
+    console.log(window.innerWidth);
+    if (carousel.isMobileView && window.innerWidth > 700) {
+        carousel.isMobileView = false;
+        carousel.populateCarousel();
+    } else if (!carousel.isMobileView && window.innerWidth < 700) {
+        carousel.isMobileView = true;
+        carousel.populateCarousel();
+    }
+    console.log(carousel.isMobileView);
+}
 
+carousel.openProjectDetails = ({currentTarget}) => {
+    const target = currentTarget.value;
+    const targetEl = document.querySelectorAll(`.projects__${target}`);
+    targetEl.forEach((target) => {
+        target.classList.add('active');
+    });
+    console.log(document.querySelector(`.projects__${target}`));
+}
 
-// carousel.changeDisplayedProject = (e) => {
-//     const carouselWidth = carousel.carouselEl.offsetWidth;
-//     switch(e.target.value) {
-//         case 'left':
-//             carousel.position += carouselWidth;
-//     } 
-// }
-
-// carousel.handleButtonClick = () => {
-//     carousel.buttonEls.forEach(button => {
-//         button.addEventListener('click', carousel.changeDisplayedProject);
-//     })
-// }
-
+carousel.createProjectButtons = () => {
+    document.querySelectorAll('.projects__open-text').forEach((button) => {
+        button.addEventListener('click', (e) => carousel.openProjectDetails(e));
+    })
+}
 
 carousel.init = () => {
-    // carousel.populateCarousel();
-    // carousel.handleButtonClick();
-    $(".owl-carousel").owlCarousel();
+    // carousel.setView();
+    // window.addEventListener('resize', carousel.setView());
+    carousel.createCarousel();
+    carousel.createProjectButtons();
 }
 
 export default carousel;
--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,6 @@ import nav from './mobile-nav.js';
 import themes from './theme-slider.js';
 import carousel from './carousel.js';
 
-$(() => {
-    nav.init();
-    themes.init();
-    carousel.init();
-});
+nav.init();
+themes.init();
+carousel.init();
